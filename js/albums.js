87f065a9fee0ef9b10629bd9e4364a3d5b9e40e9
far2na <enot.fx@gmail.com>
2017-10-02 16:30:19 +0300

    added albums page

---
+++ js/albums.js
@@ -0,0 +1,54 @@
+var gallery = {
+	albums : [
+	{
+		thumbnail : "./images/IMG_20161125_232245.jpg",
+		name : "Lying cat",
+		photos : [
+		{
+      		src: "./images/IMG_20161122_234013.jpg",
+      		title: "Resting cat"
+      	},
+      	{
+      		src: "./images/IMG_20161125_232245.jpg",
+      		title: "Sleeping cat"
+      	}
+		]
+	},
+	{
+		thumbnail : "./images/IMG_20170101_165055.jpg",
+		name : "Cat's hobby",
+		photos : [
+		{
+      		src: "./images/IMG_20161231_122656.jpg",
+      		title: "New year cat"
+      	},
+      	{
+      		src: "./images/IMG_20170101_165055.jpg",
+      		title: "Cat & puzzle"
+      	},
+      	{
+      		src: "./images/IMG_20170416_171334.jpg",
+      		title: "Cat & iron"
+      	},
+      	{
+      		src: "./images/IMG_20170428_221216.jpg",
+      		title: "Cat on the table"
+      	}
+		]
+	},
+	{
+		thumbnail : "./images/IMG_20161201_204857.jpg",
+		name : "Strange cat",
+		photos : [
+		{
+      		src: "./images/IMG_20161201_204857.jpg",
+      		title: "Frisky cat"
+      	},
+      	{
+      		src: "./images/IMG_20170810_183605.jpg",
+      		title: "O_o cat"
+      	}
+		]
+	}
+	]
+};
